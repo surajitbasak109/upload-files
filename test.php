9d0a4f37f4e475d6a5de0b3b56e724fffa469bfa
Surajit Basak <surajitbasak109@gmail.com>
2015-11-11 08:53:37 +0530

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$target = "uploads/";
-$file_name = "myFile.txt";
-$date = date( 'jmy-hms' );
-if ( !is_dir( $date ) ) mkdir( $target . "/" . $date );
-$file_path = $target . $date . "/" . $file_name;
-echo $file_path;
-?>
